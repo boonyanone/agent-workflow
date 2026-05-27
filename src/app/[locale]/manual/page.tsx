23ce636d588ce336bdb2b3e9579a07e6e3943482
boonyanone <boonyanone@gmail.com>
2026-05-27 14:14:46 +0700

    docs: update video embed link in manual

--- src/app/[locale]/manual/page.tsx
+++ src/app/[locale]/manual/page.tsx
@@ -121,7 +121,7 @@ export default function ManualPage() {
                 <div className="relative pb-[56.25%] h-0">
                   <iframe 
                     className="absolute top-0 left-0 w-full h-full"
-                    src="https://www.youtube.com/embed/lv-JEr-ZgfA" 
+                    src="https://www.youtube.com/embed/AMf-onUdjcE" 
                     title="Agent Workflow Hub Overview"
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
